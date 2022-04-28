e5df4419734ad376087fd915ff050319a7d9400c
goldcvt <thegoldcat5@gmail.com>
2022-04-28 19:11:20 +0300

    whateva

--- model.py
+++ model.py
@@ -1,6 +1,10 @@
 from datetime import date
-from typing import Optional
+from typing import List, Optional
 from dataclasses import dataclass
+
+
+class OutOfStockException(Exception):
+    pass
 
 @dataclass(frozen=True)
 class OrderLine:
@@ -9,20 +13,58 @@ class OrderLine:
     qty: int
 
 class Batch:
-    def __init__(self, available_quantity: int, sku: str, eta: Optional[date], on_warehouse: bool) -> None:
-        self.available_quantity = available_quantity
+    def __init__(self, reference: str, qty: int, sku: str, eta: Optional[date], on_warehouse: bool) -> None:
+        self.reference = reference
+        self._purchased_quantity = qty
+        self._allocations = set() # Set[OrderLine]
         self.sku = sku
         self.eta = eta
         self.on_warehouse = on_warehouse
+
+    @property
+    def available_quantity(self):
+        return self._purchased_quantity - self.allocated_quantity
+
+    @property
+    def allocated_quantity(self):
+        return sum(line.qty for line in self._allocations)
 
     def can_allocate(self, order_line: OrderLine) -> bool:
         return self.available_quantity >= order_line.qty and self.sku == order_line.sku
 
     def allocate(self, order_line: OrderLine) -> None:
         if self.can_allocate(order_line):
-            self.available_quantity -= order_line.qty
-        else:
-            raise Exception('Cannot allocate OrderLine in batch!')
+            self._allocations.add(order_line)
+    
+    def deallocate(self, order_line: OrderLine) -> None:
+        if order_line in self._allocations:
+            self._allocations.remove(order_line)
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Batch):
+            return False
+        return self.reference == other.reference
+
+    # actually objects with identity rarely need to have any __hash__
+    def __hash__(self) -> int:
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
 
 
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        used_batch = next(
+            b for b in sorted(batches) if b.can_allocate(line)
+            )
+        used_batch.allocate(line)
+        return used_batch.reference
+    except StopIteration:
+        raise OutOfStockException
 
--- test_model.py
+++ test_model.py
@@ -10,7 +10,7 @@ later = tomorrow + timedelta(days=10)
 
 def create_batch_and_order_line(sku, batch_qty, order_line_qty):
     return (
-            Batch(batch_qty, sku, today, on_warehouse=True),
+            Batch('121', batch_qty, sku, today, on_warehouse=True),
             OrderLine('Order 228', sku, order_line_qty)
             )
 
@@ -30,18 +30,52 @@ def test_cannot_allocate_if_available_smaller_than_required():
     batch, order_line = create_batch_and_order_line('red-chair', 10, 22)
     assert batch.can_allocate(order_line) == False
 
+
 def test_can_allocate_if_available_equal_to_required():
     batch, order_line = create_batch_and_order_line('red-chair', 10, 10)
     assert batch.can_allocate(order_line) == True
 
-def cannot_allocate_if_skus_dont_match():
-    batch = Batch(10, 'ololol', today, on_warehouse=True)
+
+def test_cannot_allocate_if_skus_dont_match():
+    batch = Batch('123132', 10, 'ololol', today, on_warehouse=True)
     order_line_different_sku = OrderLine('myOrder', 'black-flag', 10)
     assert batch.can_allocate(order_line_different_sku) == False
 
-def test_prefers_warehouse_batches_to_shipments():
-    pytest.fail("todo")
+
+def test_can_only_deallocate_already_allocated():
+    # probably not the smartest test ever
+    # even if deallocation actually worked, we could've seen correct qty bc dealloc is broken
+    batch, unallocated_line = create_batch_and_order_line('hot-rod', 10, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 10
 
 
-def test_prefers_earlier_batches():
-    pytest.fail("todo")
+def test_deallocation_possible():
+    batch, line = create_batch_and_order_line('hot-rod', 10, 2)
+    batch.allocate(line)
+    assert batch.available_quantity == 8
+    batch.deallocate(line)
+    assert batch.available_quantity == 10
+
+
+def test_allocation_is_idempotent():
+    batch, line = create_batch_and_order_line('hot-rod', 10, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 8
+
+def test_different_refs_batches_not_same():
+    batch = Batch('290821', 10, 'ololol', today, on_warehouse=True)
+    batch2 = Batch('123132', 123, 'ololol', today, on_warehouse=True)
+    assert (batch == batch2) is False
+
+def test_same_refs_batches_same():
+    batch = Batch('123132', 10, 'ololol', today, on_warehouse=True)
+    batch2 = Batch('123132', 123, 'ololol', today, on_warehouse=True)
+    assert (batch == batch2) is True
+
+def test_batch_and_not_batch_not_same():
+    batch = Batch('290821', 10, 'ololol', today, on_warehouse=True)
+    order_line = OrderLine('123132', '123', 213)
+    assert (batch == order_line) is False
+
